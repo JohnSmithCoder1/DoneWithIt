33d9a5d2af1645c91352a920b245b7e185536146
John Smith <johnsmithcoder1@gmail.com>
2021-10-13 10:38:21 -0500

    Wrap Image in TouchableWithoutFeedback and add onPress

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, TouchableWithoutFeedback } from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log("Text pressed");
@@ -9,15 +9,17 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress}>Hello Ubiquisoft</Text>
       <Image source={require('./assets/favicon.png')} />
-      <Image 
-        blurRadius={10}
-        fadeDuration={2000} // only works on Android
-        source={{ 
-          width: 200,
-          height: 300,
-          uri: 'https://picsum.photos/200/300' 
-        }} 
-      />
+      <TouchableWithoutFeedback onPress={() => console.log("Image pressed")}>
+        <Image 
+          blurRadius={10}
+          fadeDuration={2000} // only works on Android
+          source={{ 
+            width: 200,
+            height: 300,
+            uri: 'https://picsum.photos/200/300' 
+          }} 
+        />
+      </TouchableWithoutFeedback>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
