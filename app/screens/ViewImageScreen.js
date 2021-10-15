289b06d76b8fffc4c15f7231ded358d8340ed33b
John Smith <johnsmithcoder1@gmail.com>
2021-10-15 10:47:33 -0500

    Add placeholder delete icon

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -5,6 +5,7 @@ function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
       <View style={styles.closeIcon}></View>
+      <View style={styles.deleteIcon}></View>
       <Image 
         style={styles.image}
         source={require('../assets/chair.jpg')} 
@@ -22,6 +23,14 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 40,
     left: 30,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#4ecdc4',
+    position: 'absolute',
+    top: 40,
+    right: 30,
   },
   container: {
     backgroundColor: '#000',
