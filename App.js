6064aae18298117c0c20e1c11917a209f0e7cbb5
John Smith <johnsmithcoder1@gmail.com>
2021-10-15 10:22:54 -0500

    Set WelcomeScreen to initial screen

--- App.js
+++ App.js
@@ -1,33 +1,8 @@
 import React from 'react';
-import { Dimensions, Platform, SafeAreaView, StyleSheet, View, } from 'react-native';
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
-import { StatusBar } from 'expo-status-bar';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  // console.log(Dimensions.get('screen')); // does not respond to orientation changes
-  // console.log(useDimensions()); // does respond to orientation changes
-  console.log(useDeviceOrientation());
-
   return (
-    // SafeAreaView only works for iOS
-    <SafeAreaView style={styles.container}>
-      <View style={{
-        backgroundColor: 'dodgerblue',
-        width: '100%',
-        height: '30%',
-      }}></View>
-      <StatusBar style='auto' />
-    </SafeAreaView>
+    <WelcomeScreen />
   );
 }
-
-const containerStyle = { backgroundColor: 'lightgrey' };
-
-const styles = StyleSheet.create({ // this method validates the properties we pass (will throw error if misspelled, etc., instead of just using a regular javascript object)
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    paddingTop: Platform.OS === 'android' ? 20 : 0,
-  },
-});
